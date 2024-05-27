1666edfc7731bc7266106d59cb35d7428b110c61
davidols12 <116851084+davidols12@users.noreply.github.com>
2024-05-27 09:28:26 -0700

    sin env
    sin env

--- classes/Email.php
+++ classes/Email.php
@@ -22,11 +22,11 @@ class Email {
         $mail->isSMTP();
         $mail = new PHPMailer();
         $mail->isSMTP();
-        $mail->Host = $_ENV['EMAIL_HOST'];
+        $mail->Host = 'smtp-relay.brevo.com';
         $mail->SMTPAuth = true;
-        $mail->Port = $_ENV['EMAIL_PORT'];
-        $mail->Username = $_ENV['EMAIL_USER'];
-        $mail->Password = $_ENV['EMAIL_PASS'];
+        $mail->Port = 587;
+        $mail->Username = '75831b001@smtp-brevo.com';
+        $mail->Password = 'xsmtpsib-c90197d29676febc0fee10ffbbe26acff9ebbbeff53fcd373465ece9a3a64c1e-YfjzMZbgFk1ABIn9';
      
          $mail->setFrom('cuentas@appsalon.com');
          $mail->addAddress($this->email);
@@ -38,7 +38,7 @@ class Email {
 
          $contenido = '<html>';
          $contenido .= "<p><strong>Hola " . $this->email .  "</strong> Has Creado tu cuenta en App Salón, solo debes confirmarla presionando el siguiente enlace</p>";
-         $contenido .= "<p>Presiona aquí: <a href='" .  $_ENV['APP_URL']  . "/confirmar-cuenta?token=" . $this->token . "'>Confirmar Cuenta</a>";        
+         $contenido .= "<p>Presiona aquí: <a href='salonolivas.alwaysdata.net/confirmar-cuenta?token=" . $this->token . "'>Confirmar Cuenta</a>";        
          $contenido .= "<p>Si tu no solicitaste este cambio, puedes ignorar el mensaje</p>";
          $contenido .= '</html>';
          $mail->Body = $contenido;
@@ -53,11 +53,11 @@ class Email {
         $mail->isSMTP();
         $mail = new PHPMailer();
         $mail->isSMTP();
-        $mail->Host = $_ENV['EMAIL_HOST'];
+        $mail->Host = 'smtp-relay.brevo.com';
         $mail->SMTPAuth = true;
-        $mail->Port = $_ENV['EMAIL_PORT'];
-        $mail->Username = $_ENV['EMAIL_USER'];
-        $mail->Password = $_ENV['EMAIL_PASS'];
+        $mail->Port = 587;
+        $mail->Username = '75831b001@smtp-brevo.com';
+        $mail->Password = 'xsmtpsib-c90197d29676febc0fee10ffbbe26acff9ebbbeff53fcd373465ece9a3a64c1e-YfjzMZbgFk1ABIn9';
     
         $mail->setFrom('cuentas@appsalon.com');
         $mail->addAddress($this->email);
@@ -69,7 +69,7 @@ class Email {
 
         $contenido = '<html>';
         $contenido .= "<p><strong>Hola " . $this->nombre .  "</strong> Has solicitado reestablecer tu password, sigue el siguiente enlace para hacerlo.</p>";
-        $contenido .= "<p>Presiona aquí: <a href='" .  $_ENV['APP_URL']  . "/recuperar?token=" . $this->token . "'>Reestablecer Password</a>";        
+        $contenido .= "<p>Presiona aquí: <a href='salonolivas.alwaysdata.net/confirmar-cuenta?token=" . $this->token . "'>Reestablecer Password</a>";        
         $contenido .= "<p>Si tu no solicitaste este cambio, puedes ignorar el mensaje</p>";
         $contenido .= '</html>';
         $mail->Body = $contenido;
